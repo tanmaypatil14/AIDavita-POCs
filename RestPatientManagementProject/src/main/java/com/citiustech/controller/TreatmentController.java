be4de6c9d3addf60c87cbd92b702ff8bcb956606
Tanmay <tanmaypatil0714@gmail.com>
2024-06-13 15:30:21 +0530

    Updated rest spring boot project

--- RestPatientManagementProject/src/main/java/com/citiustech/controller/TreatmentController.java
+++ RestPatientManagementProject/src/main/java/com/citiustech/controller/TreatmentController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,14 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.citiustech.exception.TreatmentDetailsNotFoundException;
 import com.citiustech.model.PatientDemographicDetails;
 import com.citiustech.model.TreatmentDetails;
 import com.citiustech.service.TreatmentDetailsService;
 
 @RestController
 @RequestMapping(path = "/treatment")
-public class HospitalController {
+public class TreatmentController {
 
 	@Autowired
 	private TreatmentDetailsService detailsService;
